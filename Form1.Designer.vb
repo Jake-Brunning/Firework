8e4131897c8ddab58ad5f6241146ba7e6d38cd64
BrunMan <87078099+Tarmacshrimp@users.noreply.github.com>
2022-03-12 19:12:11 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Fire = New System.Windows.Forms.Button()
+        Me.IncreaseTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ColorPicker = New System.Windows.Forms.ListBox()
+        Me.Info = New System.Windows.Forms.Button()
+        Me.DrawTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Fire
+        '
+        Me.Fire.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Fire.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Fire.ForeColor = System.Drawing.Color.Crimson
+        Me.Fire.Location = New System.Drawing.Point(609, 501)
+        Me.Fire.Name = "Fire"
+        Me.Fire.Size = New System.Drawing.Size(281, 70)
+        Me.Fire.TabIndex = 0
+        Me.Fire.Text = "FIRE ROCKET"
+        Me.Fire.UseVisualStyleBackColor = False
+        '
+        'IncreaseTimer
+        '
+        Me.IncreaseTimer.Interval = 1000
+        Me.IncreaseTimer.Tag = "Increases radius"
+        '
+        'ColorPicker
+        '
+        Me.ColorPicker.BackColor = System.Drawing.Color.Black
+        Me.ColorPicker.ForeColor = System.Drawing.Color.Crimson
+        Me.ColorPicker.FormattingEnabled = True
+        Me.ColorPicker.Items.AddRange(New Object() {"Red", "Purple", "Blue", "Green", "Yellow", "Orange", "White"})
+        Me.ColorPicker.Location = New System.Drawing.Point(896, 501)
+        Me.ColorPicker.Name = "ColorPicker"
+        Me.ColorPicker.Size = New System.Drawing.Size(83, 95)
+        Me.ColorPicker.TabIndex = 1
+        '
+        'Info
+        '
+        Me.Info.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Info.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Info.ForeColor = System.Drawing.Color.Crimson
+        Me.Info.Location = New System.Drawing.Point(1411, 531)
+        Me.Info.Name = "Info"
+        Me.Info.Size = New System.Drawing.Size(122, 40)
+        Me.Info.TabIndex = 2
+        Me.Info.Text = "INFO"
+        Me.Info.UseVisualStyleBackColor = False
+        '
+        'DrawTimer
+        '
+        Me.DrawTimer.Tag = "Draws particles"
+        '
+        'Clear
+        '
+        Me.Clear.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Clear.ForeColor = System.Drawing.Color.Crimson
+        Me.Clear.Location = New System.Drawing.Point(12, 531)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(122, 40)
+        Me.Clear.TabIndex = 3
+        Me.Clear.Text = "CLEAR"
+        Me.Clear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(1545, 610)
+        Me.Controls.Add(Me.Info)
+        Me.Controls.Add(Me.ColorPicker)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Fire)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Fire As System.Windows.Forms.Button
+    Friend WithEvents IncreaseTimer As System.Windows.Forms.Timer
+    Friend WithEvents ColorPicker As System.Windows.Forms.ListBox
+    Friend WithEvents Info As System.Windows.Forms.Button
+    Friend WithEvents DrawTimer As System.Windows.Forms.Timer
+    Friend WithEvents Clear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports Microsoft.VisualBasic.PowerPacks
+Public Class Form1
+    Dim startLength As Integer = 100
+    Dim Fireworks As New List(Of Firework)
+    Private Sub Fire_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Fire.Click
+        Fireworks.Add(New Firework(startLength, ColorPicker.SelectedIndex)) 'Adding a firework to the program
+        Fireworks.Last.makeLines(Fireworks.Last)
+    End Sub
+    Private Sub Info_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Info.Click 'User info
+        MsgBox(Title:="Help", Prompt:="Press the color you want on the color list (by FIRE ROCKET) to select color." + vbNewLine + "Press fire rocket to fire a new rocket!")
+    End Sub
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        For i As Integer = 0 To (Fireworks.Count - 1) Step 1
+            Fireworks(i).clearForm(Fireworks(i))
+        Next
+        Fireworks.Clear()
+    End Sub
+End Class
+
+Class Firework 'each firework is an object of this class
+    Public xCordinate As Integer 'x cordinate of where firework starts
+    Public yCordinate As Integer 'Y cordinate of where firework starts
+    Public colour As New Color 'Pen of firework
+    Public Lines As New List(Of LineShape) 'lines array
+    Public LineLength As Integer
+    Public Sub New(ByVal startLength As String, ByVal SelectedColor As Integer) 'constructor for data about the rocket
+        LineLength = startLength
+        Select Case SelectedColor
+            Case 0
+                colour = Color.Red
+            Case 1
+                colour = Color.Purple
+            Case 2
+                colour = Color.Blue
+            Case 3
+                colour = Color.Green
+            Case 4
+                colour = Color.Yellow
+            Case 5
+                colour = Color.Orange
+            Case 6
+                colour = Color.White
+            Case Else
+                colour = Color.Blue
+        End Select
+        Randomize()
+        xCordinate = Int(Form1.Width * Rnd())
+        Randomize()
+        yCordinate = Int(Form1.Height * Rnd())
+    End Sub
+    Public Sub makeLines(ByRef firework As Firework)
+        Dim shapeCon As New ShapeContainer
+        shapeCon.Parent = Form1
+        Dim mygraphics As Graphics = Form1.CreateGraphics
+        For i As Integer = 1 To 20
+            firework.Lines.Add(New LineShape)
+        Next
+        For Each Line As LineShape In firework.Lines
+            Line.BorderColor = firework.colour
+            Line.Parent = shapeCon
+            Line.StartPoint = New System.Drawing.Point(firework.xCordinate, firework.yCordinate)
+            Dim temp As Point = generateEndpoint(firework)
+            Line.EndPoint = New System.Drawing.Point(temp)
+        Next
+    End Sub
+    Private Function generateEndpoint(ByRef firework As Firework)
+        Randomize()
+        Dim x As Decimal = Int((2 * firework.LineLength) * Rnd()) + (firework.xCordinate - firework.LineLength)
+        Dim y As Decimal = Int((2 * firework.LineLength) * Rnd()) + (firework.yCordinate - firework.LineLength)
+        Return New System.Drawing.Point(x, y)
+    End Function
+    Public Sub clearForm(ByRef firework As Firework)
+        For Each line As LineShape In firework.Lines
+            line.Dispose()
+        Next
+    End Sub
+End Class
